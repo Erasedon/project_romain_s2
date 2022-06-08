466ba12df4c59523fdb67196f73f641ffafbdf44
yanis.pirlet <yanis.pirlet@gmail.com>
2022-06-08 10:42:47 +0200

    navbar fin

---
+++ assets/include/navbar.php
@@ -0,0 +1,15 @@
+<nav id="navbar">
+        <div class="separator"></div> 
+        <a id="nav-toggle-button">
+            <span class="nav-toggle-bar1"></span>
+            <span class="nav-toggle-bar2"></span>
+            <span class="nav-toggle-bar3"></span>
+        </a>
+        <div id="nav-links">
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="inscription.php">S'incrit</a></li>
+                <li><a href="connexion.php">Connecter</a></li>
+            </ul>
+        </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link 
+        rel="stylesheet" 
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" 
+        integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" 4
+        crossorigin="anonymous" 
+        referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    
+    <title>projet projet_romain_s1</title>
+</head>
+<body>
+    <?php 
+    include 'assets/include/navbar.php';
+
+ 
+    
+    ?>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+     <script src="assets/js/navbar.js"></script>
+</body>
+</html>
